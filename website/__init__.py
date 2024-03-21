42d1e93d63208d4ad8db95421f95c9151c96cdff
JPines42 <19112@my.sanctamaria.school.nz>
2024-03-22 10:39:56 +1300

    End of session 1

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,6 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
 
 def create_app():
     app = Flask(__name__)
@@ -12,4 +14,3 @@ def create_app():
     
     
     return app
-    
--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,15 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect, url_for
 
-auth = Blueprint('auth', __name__)
+auth = Blueprint("auth", __name__)
 
-@auth.route('/login')
-def home():
-    return "<h1>TEST 1</h1>"
+@auth.route("/login")
+def login():
+    return render_template("login.html")
+
+@auth.route("/signup")
+def signup():
+    return render_template("signup.html")
+
+@auth.route("/logout")
+def logout():
+    return redirect(url_for("views.home"))
