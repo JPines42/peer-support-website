cb542b3b41667b3c66c13095e0492e49a6f0112d
JPines42 <19112@my.sanctamaria.school.nz>
2024-08-02 15:59:13 +1200

    PEP8 Redo

--- website/views.py
+++ website/views.py
@@ -1,23 +1,27 @@
 from flask import Blueprint, render_template, request, flash, jsonify
 from flask_login import login_required, current_user
 from .models import Note, Faq
-from  . import db
+from . import db
 import json
 
 views = Blueprint("views", __name__)
+
 
 @views.route("/")
 @views.route("/home")
 def home():
     return render_template("home.html", user=current_user)
 
+
 @views.route("/singdanceoff")
 def singdanceoff():
     return render_template("singdanceoff.html", user=current_user)
 
+
 @views.route("/leaders")
 def leaders():
     return render_template("leaders.html", user=current_user)
+
 
 @views.route("/faq", methods=['GET', 'POST'])
 def faq():
@@ -31,8 +35,9 @@ def faq():
             db.session.add(new_faq)
             db.session.commit()
             flash('Message submitted!', category='success')
-        
+
     return render_template("faq.html", user=current_user)
+
 
 @views.route("/notes", methods=['GET', 'POST'])
 @login_required
@@ -46,8 +51,9 @@ def notes():
             db.session.add(new_note)
             db.session.commit()
             flash('Note added!', category='success')
-        
+
     return render_template("notes.html", user=current_user)
+
 
 @views.route('/delete-note', methods=['POST'])
 def delete_note():
@@ -60,6 +66,7 @@ def delete_note():
             db.session.commit()
             flash('Note deleted!', category='success')
     return jsonify({})
+
 
 @views.route("/calendar")
 @login_required
